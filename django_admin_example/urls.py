c4f60633d93a5d7f4c21ad1c5365b258ab76de28
Jon Dufresne <jon.dufresne@gmail.com>
2020-05-10 08:43:44 -0700

    Enable translations

--- django_admin_example/urls.py
+++ django_admin_example/urls.py
@@ -1,8 +1,9 @@
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, reverse_lazy
 from django.views.generic import RedirectView
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path("", RedirectView.as_view(url=reverse_lazy("admin:index"))),
     path("admin/", admin.site.urls),
-]
+)
